7a9ff43a548d4a56e38893ae1187651d2305b720
latheef-mvmc <107998191+latheef-mvmc@users.noreply.github.com>
2026-03-30 10:17:00 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -17,7 +17,7 @@ resource "aws_vpc" "demo-vpc" {
 
 resource "aws_subnet" "demo-subnet" {
   vpc_id     = aws_vpc.demo-vpc.id
-  cidr_block = "10.0.1.0/24"
+  cidr_block = "20.0.1.0/24"
 
   tags = {
     Name = "demo-subnet-${var.env}"
